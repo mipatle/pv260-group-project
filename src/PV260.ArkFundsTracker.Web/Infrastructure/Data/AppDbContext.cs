792e98a20ebfbec542e93edd4e625d22759d37d9
Vít Janeček <xjanecek@fi.muni.cz>
2026-04-13 11:40:23 +0200

    feat(us4): Setup database connection, add init migration

---
+++ src/PV260.ArkFundsTracker.Web/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PV260.ArkFundsTracker.Web.Slices.FundPosition;
+
+namespace PV260.ArkFundsTracker.Web.Infrastructure.Data;
+
+public class AppDbContext :DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+    
+    public DbSet<FundPosition> FundPositions => Set<FundPosition>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
