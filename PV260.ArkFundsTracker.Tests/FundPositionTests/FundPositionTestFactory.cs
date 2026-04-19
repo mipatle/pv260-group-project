9bc4b0ddc5478a540fc48c6c13df9fcd5d9fb128
Vladimir Dubecky <dubecky.vd@gmail.com>
2026-04-19 13:20:37 +0200

    feat(us13): add ticker format tests

---
+++ PV260.ArkFundsTracker.Tests/FundPositionTests/FundPositionTestFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PV260.ArkFundsTracker.Web.Slices.FundPosition;
+
+namespace PV260.ArkFundsTracker.Tests.FundPositionTests;
+
+public static class FundPositionTestFactory
+{
+    public static bool IsValid(FundPosition position)
+    {
+        var context = new ValidationContext(position);
+        var results = new List<ValidationResult>();
+
+        return Validator.TryValidateObject(
+            position,
+            context,
+            results,
+            validateAllProperties: true
+        );
+    }
+
+    public static List<string> GetErrors(FundPosition position)
+    {
+        var context = new ValidationContext(position);
+        var results = new List<ValidationResult>();
+
+        Validator.TryValidateObject(
+            position,
+            context,
+            results,
+            true
+        );
+
+        return results.Select(r => r.ErrorMessage ?? "Unknown error").ToList();
+    }
+
+    public static FundPosition CreatePosition(string ticker = "ARKK", decimal shares = 1m, DateOnly? date = null)
+        => new()
+        {
+            Ticker = ticker,
+            Shares = shares,
+            Date = date ?? new DateOnly(2026, 4, 19),
+            Fund = "ARKK",
+            Company = "Test company",
+            Cusip = "123456789",
+            MarketValue = 100m,
+            WeightPercentage = 1m
+        };
+}
---
+++ PV260.ArkFundsTracker.Tests/FundPositionTests/FundPositionValidationTests.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using PV260.ArkFundsTracker.Web.Slices.FundPosition;
+
+namespace PV260.ArkFundsTracker.Tests.FundPositionTests;
+
+public class FundPositionValidationTests
+{
+    [Fact]
+    public void Ticker_HasMaxLengthTen()
+    {
+        var property = typeof(FundPosition).GetProperty(nameof(FundPosition.Ticker));
+
+        var maxLength = property?.GetCustomAttribute<MaxLengthAttribute>();
+
+        Assert.NotNull(maxLength);
+        Assert.Equal(10, maxLength.Length);
+    }
+
+    [Theory]
+    [InlineData("ARKK")]
+    [InlineData("A")]
+    [InlineData("ABCDEFGHIJ")]
+    public void Ticker_WithValidLength_PassesMaxLengthConstraint(string ticker)
+    {
+        var position = FundPositionTestFactory.CreatePosition(ticker: ticker);
+
+        Assert.True(FundPositionTestFactory.IsValid(position));
+    }
+
+    [Fact]
+    public void Ticker_WithTooLongValue_FailsMaxLengthConstraint()
+    {
+        var position = FundPositionTestFactory.CreatePosition(ticker: "ABCDEFGHIJK");
+
+        Assert.False(FundPositionTestFactory.IsValid(position));
+
+        var errors = FundPositionTestFactory.GetErrors(position);
+        Assert.NotEmpty(errors);
+        Assert.Contains(errors, e => e.Contains("Ticker"));
+    }
+}
