2aa7f62370bb70318c9dc86c02332fa9243dea15
vitjanecek <vita.jp2@seznam.cz>
2026-05-17 20:07:53 +0200

    test(us12): Add tests for Auth registration and login

---
+++ PV260.ArkFundsTracker.Tests/AuthTests/AuthTestFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PV260.ArkFundsTracker.Tests.Common;
+using PV260.ArkFundsTracker.Web.Slices.Auth.Entities;
+using PV260.ArkFundsTracker.Web.Slices.Auth.Services;
+
+namespace PV260.ArkFundsTracker.Tests.AuthTests;
+
+public class AuthTestFactory(SqliteDbFixture db)
+{
+    public AuthService CreateService()
+    {
+        return new AuthService(
+            db.Context,
+            new PasswordHasher<AppUser>());
+    }
+}
